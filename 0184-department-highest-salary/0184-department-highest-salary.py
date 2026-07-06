2f380efe5e8eb8c2e52ccf662b1fb351bf705755
Sachin Masti <114576423+sachinmasti@users.noreply.github.com>
2026-07-06 17:26:33 +0530

    Time: 312 ms (59.87%), Space: 69.2 MB (27.49%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    result = employee.merge(department,how='inner',left_on='departmentId',right_on='id')
+
+    new = result.groupby('name_y').transform('max')
+
+    filter_mask = result['salary'] == result.groupby('name_y')['salary'].transform('max')
+
+    new = result[filter_mask]
+
+    new = new.rename(columns={
+    'name_y': 'Department', 
+    'name_x': 'Employee', 
+    'salary': 'Salary'})
+
+    return new[['Department', 'Employee', 'Salary']]
